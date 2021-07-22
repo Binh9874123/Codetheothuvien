eb52c6a5191385194db7eae71f313ecbdb1ee49e
LE VU BINH <Binh9874123@gmail.com>
2021-07-22 09:09:23 -0700

    first commit

--- Codetheothuvien.py
+++ Codetheothuvien.py
@@ -5,6 +5,7 @@ from scipy import integrate
 import matplotlib.pyplot as plt
 from math import e
 from scipy import integrate as inte
+import numpy as np
 
 
 def ReadConfig(Name):
@@ -43,18 +44,17 @@ def solution(S0 , I0 , R0 , day  , h):
     y0 = [S0,I0,R0]
     y1 = SIR_model(0, y0 )
     solution = integrate.RK45(SIR_model, 0, y0 ,day ,h, 0.001, e**-6)
-    S = []
-    I = []
-    R = []
-    T = []
+    S = np.array([])
+    I = np.array([])
+    R = np.array([])
+    T = np.array([])
     while (solution.t < day ):
     # get solution step state
       solution.step()
-      T.append(solution.t)
-    #y_values.append(solution.y)
-      S.append(solution.y[0])
-      I.append(solution.y[1])
-      R.append(solution.y[2])
+      T = np.append(T , solution.t)
+      S = np.append(S , solution.y[0])
+      I = np.append(I , solution.y[1])
+      R = np.append(R , solution.y[2])
     return [S , I ,R ,T]
     
 
